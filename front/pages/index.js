32e10d8c564e1cf87d0c07f62be721ecd50ba97b
JeongbinPark <qkrwjdqls99@gmail.com>
2022-02-25 02:23:59 +0900

    fix(front): fix state's name

--- front/pages/index.js
+++ front/pages/index.js
@@ -5,12 +5,12 @@ import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
 
 const Home = () => {
-	const isLoggedIn = useSelector((state)=>state.user.isLoggedIn);
+	const me = useSelector((state)=>state.user?.me);
 	const mainPosts = useSelector((state)=>state.post.mainPosts);
 
 	return(
 		<AppLayout>
-      {isLoggedIn && <PostForm />}
+      {me && <PostForm />}
 			{mainPosts.map((post)=><PostCard key={post.id} post={post} />)}
     </AppLayout>
 	)
